dc34d699947d4717245ac55563d427d436b94047
HIMANSHU TIWARI <himanshutiwari249@gmail.com>
2026-05-22 17:11:52 +0530

    Fix container name in deployment script

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-# Log everything to start_docker.log
+
 exec > /home/ubuntu/start_docker.log 2>&1
 
-echo "Logging in to ECR..."
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 891377050051.dkr.ecr.ap-south-1.amazonaws.com
+echo "Logging into ECR..."
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 458889634556.dkr.ecr.ap-south-1.amazonaws.com
 
-echo "Pulling Docker image..."
-docker pull 891377050051.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
+echo "Pulling latest image..."
+docker pull 458889634556.dkr.ecr.ap-south-1.amazonaws.com/spotify-recommender-system:latest
 
-echo "Checking for existing container..."
-if [ "$(docker ps -q -f name=hybrid_recsys)" ]; then
-    echo "Stopping existing container..."
-    docker stop hybrid_recsys
-fi
+echo "Stopping old container..."
+docker stop spotify_recommender_system || true
 
-if [ "$(docker ps -aq -f name=hybrid_recsys)" ]; then
-    echo "Removing existing container..."
-    docker rm hybrid_recsys
-fi
+echo "Removing old container..."
+docker rm spotify_recommender_system || true
 
 echo "Starting new container..."
-docker run -d -p 80:8000 --name hybrid_recsys 891377050051.dkr.ecr.ap-south-1.amazonaws.com/spotify_hybrid_recsys:latest
+docker run -d \
+  --restart unless-stopped \
+  -p 80:8000 \
+  --name spotify_recommender_system \
+  458889634556.dkr.ecr.ap-south-1.amazonaws.com/spotify-recommender-system:latest
 
-echo "Container started successfully."
+echo "Deployment completed."
